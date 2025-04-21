5d32ad268628f63f4ce2c122472e626a212dae15
DanielFalcao <danieleducadorg@gmail.com>
2025-04-21 16:58:33 -0300

    Atualiza LayoutInicial

--- src/components/LayoutInicial.tsx
+++ src/components/LayoutInicial.tsx
@@ -1,7 +1,42 @@
+// src/components/LayoutInicial.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  background-color: ${({ theme }) => theme.colors.primary};
+  padding: ${({ theme }) => theme.spacing.medium};
+  display: flex;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spacing.large};
+  margin-bottom: ${({ theme }) => theme.spacing.large};
+`;
+
+const NavLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.white};
+  text-decoration: none;
+  font-weight: 600;
+  &:hover {
+    color: ${({ theme }) => theme.colors.secondary};
+  }
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
 const LayoutInicial: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <Container>
+      <Nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/ranking">Ranking</NavLink>
+        <NavLink to="/cadastro">Cadastrar Aluno</NavLink>
+      </Nav>
+      {children}
+    </Container>
+  );
 };
 
 export default LayoutInicial;
